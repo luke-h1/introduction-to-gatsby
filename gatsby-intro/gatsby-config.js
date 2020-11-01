608d1113546a8e84f6ec646f43871987df11acd7
luke-h1 <luke.howsam@yahoo.com>
2020-11-01 21:09:39 +0000

    mdx setup

--- gatsby-intro/gatsby-config.js
+++ gatsby-intro/gatsby-config.js
@@ -1,8 +1,20 @@
 module.exports = {
   siteMetadata: {
     title: 'Frontend Masters Gatsby Workshop',
-    description:'A site we built together during a full-day Frontend Masters Gatsby workshop',
+    description:
+      'A site we built together during a full-day Frontend Masters Gatsby workshop',
   },
 
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
