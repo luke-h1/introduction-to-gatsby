e9c34d0ad9b1034257bccc07feb8d7929d3d6dac
luke-h1 <luke.howsam@yahoo.com>
2020-11-01 21:31:30 +0000

    map thru blog post

---
+++ gatsby-intro/src/components/post-preview.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { css } from '@emotion/core';
+import { Link } from 'gatsby';
+
+const PostPreview = ({ post }) => (
+  <article>
+    <h3>
+      <Link to={post.slug}>{post.title}</Link>
+    </h3>
+    <p>{post.excerpt}</p>
+    <Link to={post.slug}>Read this post &rarr;</Link>
+  </article>
+);
+export default PostPreview;
--- gatsby-intro/src/pages/index.js
+++ gatsby-intro/src/pages/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello Minnesota</p>
-    <Link to="/about">Learn About Me &rarr;</Link>
-  </Layout>
-);
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
+export default () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello Minnesota</p>
+      <Link to="/about">Learn About Me &rarr;</Link>
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        // <pre>{JSON.stringify(post, null, 2)}</pre>
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
