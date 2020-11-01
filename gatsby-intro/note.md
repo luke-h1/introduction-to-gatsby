f996af2a07974df9d951894fcce570201609d260
luke-h1 <luke.howsam@yahoo.com>
2020-11-01 22:08:57 +0000

    getting data by slug

---
+++ gatsby-intro/note.md
@@ -0,0 +1,1 @@
+## on lesson getting posts Q&A 
--- gatsby-intro/src/templates/post.js
+++ gatsby-intro/src/templates/post.js
@@ -4,7 +4,6 @@ import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { css } from '@emotion/core';
 import Layout from '../components/layout';
 import ReadLink from '../components/read-link';
-
 export const query = graphql`
   query($slug: String!) {
     mdx(frontmatter: { slug: { eq: $slug } }) {
